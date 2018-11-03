6bccf2738b70843d3567012e5c95392bcbce2d89
wtx20074587 <313659532@qq.com>
2018-11-03 08:20:26 +0800

    1103-update perceptron
    update

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -11,9 +11,9 @@ def cacl_y(w, x, b):
 # 感知机计算过程
 def perceptron(data_coord, data_label):
     # 0. 初始化参数:w,b, learning_rate
-    learning_rate = 0.001
-    w_star = np.zeros(shape=data_coord[0].shape) + 0.1  # no zeros
-    b_star = 0.1
+    learning_rate = 1
+    w_star = np.zeros(shape=data_coord[0].shape) #zeros
+    b_star = 0
 
     #1.开启更新w，b的循环:
     # 假设没有不可分的数据，当全部数据分类正确时，停止循环
@@ -24,13 +24,16 @@ def perceptron(data_coord, data_label):
             x = data_coord[i]
             y_ = data_label[i]
             y = cacl_y(w_star, x, b_star)
-            # print("y_ = ", y_)
-            # print("y = ", y)
+            print("y_ = ", y_)
+            print("y = ", y)
             print("\n\n")
-            # 2.2 若分类错误，更新w，b
-            if y * y_ < 0:  # update w,b
+            # 2.2 若分类错误(不大于0)，更新w，b
+            if y * y_ <= 0:  # update w,b
                 w_star += learning_rate * y_ * x
                 b_star += learning_rate * y_
+                print("w_star = ",w_star)
+                print("b_star = ",b_star)
+
             # 2.2 分类正确，对分类正确的数据个数 计数
             else:
                 print("count = ", count)
@@ -50,7 +53,7 @@ def perceptron(data_coord, data_label):
     print("b_star = ", b_star)
 
 # 准备3组测试数据，2个正例， 1个负例
-data_coord = np.asarray(((-3, 3), (-4, 3), (5, 5)))
+data_coord = np.asarray(((3, 3), (4, 3), (1, 1)))
 data_label = np.asarray((1, 1, -1))
 # start perceptron
 perceptron(data_coord, data_label)
